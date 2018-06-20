8ce6b73873583baf34492e1bfaf3399519ba1d72
Ivan Zhang <ivanzhangemail@gmail.com>
2018-06-19 20:04:30 -0400

    Added Fragment Changes

--- app/src/main/java/app/izhang/kedditbysteps/MainActivity.kt
+++ app/src/main/java/app/izhang/kedditbysteps/MainActivity.kt
@@ -2,6 +2,8 @@ package app.izhang.kedditbysteps
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
@@ -10,6 +12,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    // Kotlin is a concise, safe, statically typed programming language focused on interoperability with Java
+    /**
+     * 1. No primitive types, double is a "Double" object, some of the code will tranform into primitive types by the compiler for performance.
+     *
+     */
+
+    /* Can also say 'override fun onCreate(savedInstanceState: Bundle?) : Unit {' to define a "void" return value */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,6 +27,50 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+        }
+
+        /**
+         * Everything in Kotlin is not nullable unless you specifically declare it.
+         * Ex) val a : String = null // This will not compile
+         * Ex) val a : String? = null // Works since you are indicating the value can be null using the '?'
+         *
+         */
+
+        if(savedInstanceState == null){
+           // changeFragment(NewsFragment())
+        }
+
+    }
+
+    fun changeFragment(f: Fragment, cleanStack: Boolean = false) {
+        val ft = supportFragmentManager.beginTransaction()
+        if (cleanStack) {
+            clearBackStack()
+        }
+        ft.setCustomAnimations(
+                R.anim.abc_fade_in, R.anim.abc_fade_out, R.anim.abc_popup_enter, R.anim.abc_popup_exit)
+        ft.replace(R.id.activity_base_content, f)
+        ft.addToBackStack(null)
+        ft.commit()
+    }
+
+    fun clearBackStack() {
+        val manager = supportFragmentManager
+        if (manager.backStackEntryCount > 0) {
+            val first = manager.getBackStackEntryAt(0)
+            manager.popBackStack(first.id, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+        }
+    }
+
+    /**
+     * Finish activity when reaching the last fragment.
+     */
+    override fun onBackPressed() {
+        val fragmentManager = supportFragmentManager
+        if (fragmentManager.backStackEntryCount > 1) {
+            fragmentManager.popBackStack()
+        } else {
+            finish()
         }
     }
 
